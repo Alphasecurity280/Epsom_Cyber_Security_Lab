d3997b98b3239b84a6c47e9511511053c4cd381c
Alphasecurity280 <Alphasecurity280@proton.me>
2026-07-20 03:38:42 -0400

    Add my first Python Cybersecurity tool

---
+++ cyber_tool.py
@@ -0,0 +1,6 @@
+print("====================================")
+print(" DUBAI CYBERSECURITY ACADEMY TOOL ")
+print("====================================")
+agent_name = input("Enter your Agent Name:")
+print(f"Welcome back,Agent {agent_name}! Your system is fully secured.")
+
