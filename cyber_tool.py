66f0bddbfe92bea199ee90cc79c499cb0eba1dab
Alphasecurity280 <Alphasecurity280@proton.me>
2026-07-23 17:19:52 -0400

    Update cyber_tool banner to Epsom Malaysia

--- cyber_tool.py
+++ cyber_tool.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3
 print("====================================")
-print(" DUBAI CYBERSECURITY ACADEMY TOOL ")
+print(" Epsom_Cyber_Security_Lab ")
 print("====================================")
 agent_name = input("Enter your Agent Name:")
 print(f"Welcome back,Agent {agent_name}! Your system is fully secured.")
